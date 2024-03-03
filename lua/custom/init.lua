a3ac99618639746a3df3e099d6458f77508e6dba
Filip Kalný <filip@kalny.net>
2024-03-03 14:40:54 +0100

    added cd on folder open

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -2,3 +2,13 @@ vim.opt.relativenumber = true
 
 -- Copilot
 vim.g.copilot_assume_mapped = true
+
+-- CD opening with a commandline path
+local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
+  end,
+})
