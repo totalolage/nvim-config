e4e659865f5573bdbab891baff63e6622632c865
Filip Kalný <filip@kalny.net>
2024-04-19 10:25:21 +0200

    add highlight on yank

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -10,4 +10,19 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
+-- Highlight on yank
+
+-- Vimscript legacy
+-- augroup highlight_yank
+--     autocmd!
+--     autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank("IncSearch", 1000)
+-- augroup END
+
+-- lua
+vim.api.nvim_exec(
+  [[
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup=(vim.fn['hlexists']('HighlightedyankRegion') > 0 and 'HighlightedyankRegion' or 'IncSearch'), timeout=300}
+  ]],
+  false
+)
 
