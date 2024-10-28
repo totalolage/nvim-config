c7a0e0f7925880ad752cb65c189cc3daf803091f
Filip Kalný <c_fkalny@groupon.com>
2024-10-28 13:42:01 +0100

    restore reset hunk

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,11 +2,6 @@ return {
   "lewis6991/gitsigns.nvim",
   keys = {
     {
-      "<leader>rb",
-      "<cmd>lua require('gitsigns').reset_buffer()<CR>",
-      desc = "Reset buffer",
-    },
-    {
       "<leader>sh",
       "<cmd>lua require('gitsigns').stage_hunk()<CR>",
       desc = "Stage hunk",
@@ -17,6 +12,11 @@ return {
       desc = "Undo stage hunk",
     },
     {
+      "<leader>rh",
+      "<cmd>lua require('gitsigns').reset_hunk()<CR>",
+      desc = "Reset hunk",
+    },
+    {
       "<leader>sb",
       "<cmd>lua require('gitsigns').stage_buffer()<CR>",
       desc = "Stage buffer",
@@ -25,6 +25,11 @@ return {
       "<leader>ub",
       "<cmd>lua require('gitsigns').reset_buffer_index()<CR>",
       desc = "Undo stage buffer",
-    }
+    },
+    {
+      "<leader>rb",
+      "<cmd>lua require('gitsigns').reset_buffer()<CR>",
+      desc = "Reset buffer",
+    },
   }
 }
