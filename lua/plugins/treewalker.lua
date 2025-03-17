2e9f1610675a2a57163fa2278322aa4c8e6d9052
Filip Kalný <c_fkalny@groupon.com>
2025-03-17 21:48:43 +0100

    add treewalker

---
+++ lua/plugins/treewalker.lua
@@ -0,0 +1,40 @@
+return {
+  'aaronik/treewalker.nvim',
+
+  -- The following options are the defaults.
+  -- Treewalker aims for sane defaults, so these are each individually optional,
+  -- and setup() does not need to be called, so the whole opts block is optional as well.
+  -- opts = {
+  --   -- Whether to briefly highlight the node after jumping to it
+  --   highlight = true,
+  --
+  --   -- How long should above highlight last (in ms)
+  --   highlight_duration = 250,
+  --
+  --   -- The color of the above highlight. Must be a valid vim highlight group.
+  --   -- (see :h highlight-group for options)
+  --   highlight_group = 'CursorLine',
+  -- }
+  -- -- movement
+  -- vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', { silent = true })
+  -- vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', { silent = true })
+  -- vim.keymap.set({ 'n', 'v' }, '<C-h>', '<cmd>Treewalker Left<cr>', { silent = true })
+  -- vim.keymap.set({ 'n', 'v' }, '<C-l>', '<cmd>Treewalker Right<cr>', { silent = true })
+  --
+  -- -- swapping
+  -- vim.keymap.set('n', '<C-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
+  -- vim.keymap.set('n', '<C-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
+  -- vim.keymap.set('n', '<C-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
+  -- vim.keymap.set('n', '<C-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
+  cmd = "Treewalker",
+  keys = {
+    { "<C-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" }, "Treewalker up" },
+    { "<C-j>", "<cmd>Treewalker Down<cr>", mode = { "n", "v" }, "Treewalker down" },
+    { "<C-h>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" }, "Treewalker left" },
+    { "<C-l>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" }, "Treewalker right" },
+    { "<C-S-k>", "<cmd>Treewalker SwapUp<cr>", mode = "n", desc = "Swap with the node above" },
+    { "<C-S-j>", "<cmd>Treewalker SwapDown<cr>", mode = "n", desc = "Swap with the node below" },
+    { "<C-S-h>", "<cmd>Treewalker SwapLeft<cr>", mode = "n", desc = "Swap with the node to the left" },
+    { "<C-S-l>", "<cmd>Treewalker SwapRight<cr>", mode = "n", desc = "Swap with the node to the right" },
+  },
+}
