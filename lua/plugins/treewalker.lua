f94e81335ca7c7b54435a1fa931d23e7abc0d99d
Filip Kalný <c_fkalny@groupon.com>
2025-03-25 10:28:33 +0100

    remove old treewalker keybinds

--- lua/plugins/treewalker.lua
+++ lua/plugins/treewalker.lua
@@ -15,17 +15,6 @@ return {
   --   -- (see :h highlight-group for options)
   --   highlight_group = 'CursorLine',
   -- }
-  -- -- movement
-  -- vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', { silent = true })
-  -- vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', { silent = true })
-  -- vim.keymap.set({ 'n', 'v' }, '<C-h>', '<cmd>Treewalker Left<cr>', { silent = true })
-  -- vim.keymap.set({ 'n', 'v' }, '<C-l>', '<cmd>Treewalker Right<cr>', { silent = true })
-  --
-  -- -- swapping
-  -- vim.keymap.set('n', '<C-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
-  -- vim.keymap.set('n', '<C-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
-  -- vim.keymap.set('n', '<C-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
-  -- vim.keymap.set('n', '<C-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
   cmd = "Treewalker",
   keys = {
     { "<C-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" }, desc = "Treewalker up" },
