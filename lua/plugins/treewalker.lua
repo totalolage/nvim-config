f0bb48abbbb85c9482f835a0ba6960c20218ffb5
Filip Kalný <c_fkalny@groupon.com>
2025-05-08 11:07:28 +0200

    revert treewalker

--- lua/plugins/treewalker.lua
+++ lua/plugins/treewalker.lua
@@ -15,15 +15,26 @@ return {
   --   -- (see :h highlight-group for options)
   --   highlight_group = 'CursorLine',
   -- }
+  -- -- movement
+  -- vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', { silent = true })
+  -- vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', { silent = true })
+  -- vim.keymap.set({ 'n', 'v' }, '<C-h>', '<cmd>Treewalker Left<cr>', { silent = true })
+  -- vim.keymap.set({ 'n', 'v' }, '<C-l>', '<cmd>Treewalker Right<cr>', { silent = true })
+  --
+  -- -- swapping
+  -- vim.keymap.set('n', '<C-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
+  -- vim.keymap.set('n', '<C-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
+  -- vim.keymap.set('n', '<C-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
+  -- vim.keymap.set('n', '<C-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
   cmd = "Treewalker",
   keys = {
-    { "<A-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" }, desc = "Treewalker up" },
-    { "<A-j>", "<cmd>Treewalker Down<cr>", mode = { "n", "v" }, desc = "Treewalker down" },
-    { "<A-h>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" }, desc = "Treewalker left" },
-    { "<A-l>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" }, desc = "Treewalker right" },
-    { "<A-S-k>", "<cmd>Treewalker SwapUp<cr>", mode = "n", desc = "Swap with the node above" },
-    { "<A-S-j>", "<cmd>Treewalker SwapDown<cr>", mode = "n", desc = "Swap with the node below" },
-    { "<A-S-h>", "<cmd>Treewalker SwapLeft<cr>", mode = "n", desc = "Swap with the node to the left" },
-    { "<A-S-l>", "<cmd>Treewalker SwapRight<cr>", mode = "n", desc = "Swap with the node to the right" },
+    { "<C-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" }, desc = "Treewalker up" },
+    { "<C-j>", "<cmd>Treewalker Down<cr>", mode = { "n", "v" }, desc = "Treewalker down" },
+    { "<C-h>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" }, desc = "Treewalker left" },
+    { "<C-l>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" }, desc = "Treewalker right" },
+    { "<C-S-k>", "<cmd>Treewalker SwapUp<cr>", mode = "n", desc = "Swap with the node above" },
+    { "<C-S-j>", "<cmd>Treewalker SwapDown<cr>", mode = "n", desc = "Swap with the node below" },
+    { "<C-S-h>", "<cmd>Treewalker SwapLeft<cr>", mode = "n", desc = "Swap with the node to the left" },
+    { "<C-S-l>", "<cmd>Treewalker SwapRight<cr>", mode = "n", desc = "Swap with the node to the right" },
   },
 }
